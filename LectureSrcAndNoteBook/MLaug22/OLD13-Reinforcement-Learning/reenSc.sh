e4f9630c0db0a02f4857ab6d5609c9f17a4d1447
Ati chets <ksde34@gmail.com>
2022-11-21 11:31:20 +0700

    update reenlab

---
+++ LectureSrcAndNoteBook/MLaug22/OLD13-Reinforcement-Learning/reenSc.sh
@@ -0,0 +1,12 @@
+pip3 install cmake;
+pip3 install ale-py;
+pip3 install autorom;
+AutoROM;
+
+ale-import-roms /usr/local/lib/python3.10/dist-packages/AutoROM/roms;
+pip3 install gym[all];
+
+pip install gym[toy_text];
+
+
+pip install gym[box2d];
